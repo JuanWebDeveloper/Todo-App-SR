90f08c94a4d6b80c912dcd3daaee6e6cd1cf73ba
Juan Vargas <juandeveloper19@gmail.com>
2022-05-05 19:46:41 -0500

    Store configuration for the application frontend

---
+++ frontend/src/store/appReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  todos: [],
+  isEditing: {},
+  todoSearch: [],
+  isSearching: false,
+};
+
+export const appReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
