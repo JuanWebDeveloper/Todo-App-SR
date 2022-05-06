68b1de088f0c9b82f37ed6c375a18712f197f6ac
Juan Vargas <juandeveloper19@gmail.com>
2022-05-05 19:38:10 -0500

    Search and list from to-do

---
+++ frontend/src/components/listTodo/SearchTodo.jsx
@@ -0,0 +1,9 @@
+export const SearchTodo = () => {
+  return (
+    <div className='search-todo'>
+      <form>
+        <input type='text' name='search' placeholder='Search to-do' />
+      </form>
+    </div>
+  );
+};
--- frontend/src/containers/Main.js
+++ frontend/src/containers/Main.js
@@ -1,5 +1,6 @@
 // Containers
 import Layouts from './Layouts';
+import ListTodo from './ListTodo';
 import Modals from './Modals';
 
 const Main = () => {
@@ -7,6 +8,7 @@ const Main = () => {
     <div className='main'>
       <Layouts>
         <div className='content'>
+          <ListTodo />
           <Modals />
         </div>
       </Layouts>
