39c526aee4205c3d13c86e5360ce0d151b6b3215
Juan Vargas <juandeveloper19@gmail.com>
2022-05-05 20:25:16 -0500

    The entity where the to-dos will be saved was created

---
+++ api/src/main/java/com/todo/app/api/model/TodoModel.java
@@ -0,0 +1,48 @@
+package com.todo.app.api.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class TodoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+    private String todoDescription;
+    private boolean isCompleted;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTodoDescription() {
+        return todoDescription;
+    }
+
+    public void setTodoDescription(String todoDescription) {
+        this.todoDescription = todoDescription;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    // Constructors
+    public TodoModel(String todoDescription, boolean isCompleted) {
+        this.todoDescription = todoDescription;
+        this.isCompleted = isCompleted;
+    }
+
+    public TodoModel() {
+    }
+}
